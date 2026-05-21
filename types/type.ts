ab503092c1fc8278c957f9f6bb47fbea965ef8f0
winfredo <winfrednukpe2002@gmail.com>
2026-05-21 11:53:21 +0000

    typescript types.

---
+++ types/type.ts
@@ -0,0 +1,62 @@
+import { ReactNode } from "react";
+
+export type BookItem = {
+  id: number;
+  title: string;
+  author: string;
+  year: number;
+  genre: string;
+  img: string;
+  status?: "available" | "borrowed";
+  dueDate?: string;
+};
+
+export type CirculationItem = {
+  initials: string;
+  name: string;
+  book: string;
+  borrowDate: string;
+  dueDate: string;
+  status: "borrowed" | "overdue";
+};
+
+export type NavItem = {
+  label: string;
+  icon: ReactNode;
+  active: boolean;
+};
+
+export type HowItWorksStep = {
+  num: string;
+  title: string;
+  desc: string;
+};
+
+export type Testimonial = {
+  name: string;
+  role: string;
+  quote: string;
+  img: string;
+  rating: number;
+  icon: ReactNode;
+};
+
+export type Book = {
+  id: number;
+  title: string;
+  author: string;
+  genre: string;
+  year: number;
+  status: "available" | "borrowed";
+  img: string;
+};
+
+export type BorrowedBook = {
+    id: number;
+    title: string;
+    author: string;
+    dueDate: string;
+    status: "active" | "overdue";
+    img: string;
+    
+}
