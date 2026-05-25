3890310235e96eed0b692b15740c787694ef5703
winfredo <winfrednukpe2002@gmail.com>
2026-05-25 15:42:53 +0000

    axios config.

---
+++ lib/axios.ts
@@ -0,0 +1,32 @@
+import axios, { InternalAxiosRequestConfig, AxiosError } from "axios";
+import { getCookieValue, clearAuthCookies } from "./auth";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// attach token to every request automatically
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  const token = getCookieValue("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// handle responses and errors
+api.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      clearAuthCookies();
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
