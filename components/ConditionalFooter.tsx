fc0417b501c4f4fd20324710e777b57801ae6805
winfredo <winfrednukpe2002@gmail.com>
2026-05-25 15:42:17 +0000

    remove the footer from the login page and admin page.

--- components/ConditionalFooter.tsx
+++ components/ConditionalFooter.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 export default function ConditionalFooter() {
   const pathname = usePathname();
 
-  if (!pathname || pathname.startsWith("/admin")) {
+  if (!pathname || pathname.startsWith("/dashboard/librarian") || pathname === "/login"  ) {
     return null;
   }
 
